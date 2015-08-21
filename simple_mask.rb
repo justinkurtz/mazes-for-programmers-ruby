ac3d25f4d7463483edd1784e3bef0a392061bb0e
Justin Kurtz <kurtz.justin@gmail.com>
2015-08-20 20:11:53 -0600

    Add a masking grid

---
+++ simple_mask.rb
@@ -0,0 +1,14 @@
+require "./mask"
+require "./masked_grid"
+require "./recursive_backtracker"
+
+mask = Mask.new(5, 5)
+
+mask[0, 0] = false
+mask[2, 2] = false
+mask[4, 4] = false
+
+grid = MaskedGrid.new(mask)
+RecursiveBacktracker.on(grid)
+
+puts grid
